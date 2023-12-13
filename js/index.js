8af393ef8652394d6da431555ea41258e7fc1ec6
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-13 12:48:51 +0100

    add solutions

--- js/index.js
+++ js/index.js
@@ -4,10 +4,19 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+addButton.addEventListener("click", (event) => {
+  
+  console.log("funktioniert");
+  event.preventDefault();
+  const newToastMessage = document.createElement("li");
+  newToastMessage.classList.add("toast-container__message");
+  toastContainer.append(newToastMessage);
+  newToastMessage.textContent = ("mhh toast");
+  console.log(newToastMessage);
 });
 
+
 clearButton.addEventListener("click", () => {
+  toast-container__message.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
